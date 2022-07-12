cb49caf652446c2fe34b17c1a6b4214b8019059d
socr102 <ericdickersonfree127@gmail.com>
2022-07-12 16:34:07 +0900

    connect metamask

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,52 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc"
+import { WagmiConfig, createClient, configureChains, chain } from "wagmi"
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { useState, useEffect} from 'react'
+
+const { provider } = configureChains(
+  [chain.mainnet],
+  [
+      jsonRpcProvider({
+        rpc: () => {
+          return {
+            http: "http://eth-mainnet.blastapi.io/API-KEY",
+          };
+        },
+      }),
+  ]
+);
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({
+      chains: [chain.mainnet],
+    }),
+  ],
+  provider,
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [showChild, setShowChild] = useState<boolean>(false);
+
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+  if (typeof window === 'undefined') {
+    return <></>;
+  } else {
+    return (
+      <WagmiConfig client={client}>
+        <Component {...pageProps}/>
+      </WagmiConfig>
+    );
+  }
 }
 
 export default MyApp
